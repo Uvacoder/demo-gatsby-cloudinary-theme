7ac6fdbf3b892a5a8001682edfc1473880ebdac0
Uvacoder <uva22940coder@gmail.com>
2022-09-27 21:14:23 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       resolve: "gatsby-theme-cloudinary-gallery",
       options: {
-        basePath:"/gallery",
+        basePath:"/profilepics'",
         cloudName: process.env.CLOUDINARY_CLOUDNAME,
         apiKey: process.env.CLOUDINARY_API_KEY,
         apiSecret: process.env.CLOUDINARY_API_SECRET,
