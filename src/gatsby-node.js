efaa3ebd7c4c30d4e99c945a685bac70f4586154
Uvacoder <uva22940coder@gmail.com>
2022-09-27 21:07:15 -0400

    Delete gatsby-node.js

--- src/gatsby-node.js
+++
@@ -1,9 +0,0 @@
-
-exports.createPages = async ({ actions }) => {
-    const basePath = "/";
-  
-    actions.createPage({
-      path: basePath,
-      component: require.resolve("./src/pages/index.js"),
-    });
-  };
