b69a6767cffd2938c9ae375a4d307ca97f85844f
Uvacoder <uva22940coder@gmail.com>
2022-09-27 21:53:53 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
         apiKey: process.env.CLOUDINARY_API_KEY,
         apiSecret: process.env.CLOUDINARY_API_SECRET,
         type: 'upload',
-        prefix:'gatsby-theme-cloudinary-gallery',
         maxResults:15
       },
     },
